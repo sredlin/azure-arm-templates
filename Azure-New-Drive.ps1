33eb12e71eea05357cdb5f0b446027b92edeefe1
Stefan Redlin <sredlin@vater-gruppe.de>
2018-03-02 17:15:48 +0100

    Add files via upload

---
+++ Azure-New-Drive.ps1
@@ -0,0 +1,25 @@
+﻿param (
+    [string]$DriveName,
+    [string]$DriveLetter
+)
+
+New-StoragePool -FriendlyName "$DriveName" `
+-PhysicalDisks (Get-PhysicalDisk -CanPool $true) `
+-StorageSubSystemFriendlyName (Get-StorageSubSystem -FriendlyName *).FriendlyName
+
+New-VirtualDisk -FriendlyName "$DriveName" `
+-StoragePoolFriendlyName "$DriveName" `
+-ProvisioningType Thin `
+-ResiliencySettingName Simple `
+-Size (Get-StoragePool –FriendlyName "$DriveName").Size 
+
+Get-VirtualDisk  | Get-Disk |Initialize-Disk|New-Partition -UseMaximumSize -AssignDriveLetter
+Get-VirtualDisk  | Get-Disk |New-Partition -UseMaximumSize -AssignDriveLetter -DriveLetter 
+
+Get-Volume | where {$_.OperationalStatus -eq "unknown" -and $_.DriveType -eq "Fixed"}|`
+Format-Volume -FileSystem NTFS -NewFileSystemLabel "$DriveName"
+
+
+
+
+

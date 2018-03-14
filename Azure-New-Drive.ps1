398d9ccc2ccf8f2b4ff5935233d5be924278fbea
StefanRedlin <sredlin@vater-gruppe.de>
2018-03-14 18:53:01 +0100

    Driveletter

--- Azure-New-Drive.ps1
+++ Azure-New-Drive.ps1
@@ -4,13 +4,13 @@ Param (
  )
 
 $DriveName = "ADDS"
-$DriveLetter = "F"
+$DriveLetter = "X"
 
 
 New-StoragePool -FriendlyName "$DriveName" -PhysicalDisks (Get-PhysicalDisk -CanPool $true) -StorageSubSystemFriendlyName (Get-StorageSubSystem -FriendlyName *).FriendlyName
 New-VirtualDisk -FriendlyName "$DriveName" -StoragePoolFriendlyName "$DriveName" -ProvisioningType Thin -ResiliencySettingName Simple -Size (Get-StoragePool -FriendlyName "$DriveName").Size 
 Get-VirtualDisk  | Get-Disk |Initialize-Disk|New-Partition -UseMaximumSize -AssignDriveLetter
-Get-VirtualDisk  | Get-Disk |New-Partition -UseMaximumSize -AssignDriveLetter -DriveLetter 
+Get-VirtualDisk  | Get-Disk |New-Partition -UseMaximumSize -DriveLetter $DriveLetter 
 Get-Volume | where {$_.OperationalStatus -eq "unknown" -and $_.DriveType -eq "Fixed"}|Format-Volume -FileSystem NTFS -NewFileSystemLabel "$DriveName"
 
 

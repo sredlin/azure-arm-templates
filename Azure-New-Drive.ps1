0d0816467eeca1f2313573777ae69b1c28614bbb
StefanRedlin <sredlin@vater-gruppe.de>
2018-03-14 19:19:55 +0100

    powershellscript angepasst

--- Azure-New-Drive.ps1
+++ Azure-New-Drive.ps1
@@ -9,8 +9,10 @@ $DriveLetter = "X"
 
 New-StoragePool -FriendlyName "$DriveName" -PhysicalDisks (Get-PhysicalDisk -CanPool $true) -StorageSubSystemFriendlyName (Get-StorageSubSystem -FriendlyName *).FriendlyName
 New-VirtualDisk -FriendlyName "$DriveName" -StoragePoolFriendlyName "$DriveName" -ProvisioningType Thin -ResiliencySettingName Simple -Size (Get-StoragePool -FriendlyName "$DriveName").Size 
-Get-VirtualDisk  | Get-Disk |Initialize-Disk|New-Partition -UseMaximumSize -AssignDriveLetter
+Get-VirtualDisk  | Get-Disk |Initialize-Disk -
+
 Get-VirtualDisk  | Get-Disk |New-Partition -UseMaximumSize -DriveLetter $DriveLetter 
+
 Get-Volume | where {$_.OperationalStatus -eq "unknown" -and $_.DriveType -eq "Fixed"}|Format-Volume -FileSystem NTFS -NewFileSystemLabel "$DriveName"
 
 
